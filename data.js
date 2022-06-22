aba14e8b7b214254d4c209309dfdb8e4d1de11ff
opadam <54212616+opaDam@users.noreply.github.com>
2022-06-22 23:23:15 +0200

    Update data.js

--- data.js
+++ data.js
@@ -3,31 +3,31 @@ let songs = [
         name: 'Candida',
         path: 'https://opadam.github.io/map/map/a10',
         artist: 'Tony Orlando',
-        cover: 'images/cover 1.png'
+        cover: 'images/singel.png'
     },
     {
         name: 'Come A Little Bit Closer',
         path: 'https://opadam.github.io/map/map/a9',
         artist: 'Jay & the Americans',
-        cover: 'images/cover 2.png'
+        cover: 'images/singel.png'
     },
     {
         name: 'Feel Free',
         path: 'https://opadam.github.io/map/map/a8',
         artist: 'Bellamy Brothers',
-        cover: 'images/cover 3.png'
+        cover: 'images/singel.png'
     },
     {
         name: 'Dance For Ever More',
         path: 'https://opadam.github.io/map/map/a7',
         artist: 'Si Cranstoun',
-        cover: 'images/cover 4.png'
+        cover: 'images/singel.png'
     },
     {
         name: 'You Drive Me Crazy',
         path: 'https://opadam.github.io/map/map/a6',
         artist: 'Shakin Stevens',
-        cover: 'images/cover 5.png'
+        cover: 'images/singel.png'
     },
 ]
 
