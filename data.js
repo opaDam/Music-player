b496669ea149b4d343a5acc771049b0485d2d59f
opadam <adam.broeken@gmail.com>
2022-08-20 00:08:25 +0200

    bell

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ let songs = [
         name: 'Feel Free',
         path: 'https://opadam.github.io/map/map/a8',
         artist: 'Bellamy Brothers',
-        cover: 'https://opadam.github.io/map/img/a8'
+        cover: 'https://opadam.github.io/map/img/Bellamy.jpg'
     },
     {
         name: 'Candida',
