f822067dc1effd489494fc8a2c6914d631aadd62
opadam <adam.broeken@gmail.com>
2022-08-20 18:28:04 +0200

    com2

--- data.js
+++ data.js
@@ -15,49 +15,49 @@ let songs = [
         name: 'Come A Little Bit Closer',
         path: 'https://opadam.github.io/map/map/a9',
         artist: 'Jay & the Americans',
-        cover: 'images/Come_a_Little_Bit_Closer_Single.jpg'
+        cover: 'https://opadam.github.io/map/img/Come_a_Little_Bit_Closer_Single.jpg'
     },
     {
         name: 'Dance For Ever More',
         path: 'https://opadam.github.io/map/map/a7',
         artist: 'Si Cranstoun',
-        cover: 'images/Si Cranstoun.jpg'
+        cover: 'https://opadam.github.io/map/img/Si Cranstoun.jpg'
     },
     {
         name: 'You Drive Me Crazy',
         path: 'https://opadam.github.io/map/map/a6',
         artist: 'Shakin Stevens',
-        cover: 'images/Shakin Stevens.jpg'
+        cover: 'https://opadam.github.io/map/img/Shakin Stevens.jpg'
     },
     {
         name: 'Take on Me',
         path: 'https://opadam.github.io/map/map/a1',
         artist: 'A-ha',
-        cover: 'images/Take on Me.jpg'
+        cover: 'https://opadam.github.io/map/img/Take on Me.jpg'
     },
     {
         name: 'You (DU)',
         path: 'https://opadam.github.io/map/map/a2',
         artist: 'Bouke',
-        cover: 'images/You (DU).jpg'
+        cover: 'https://opadam.github.io/map/img/You (DU).jpg'
     },
     {
         name: 'Country Boy',
         path: 'https://opadam.github.io/map/map/a3',
         artist: 'Alan Jackson',
-        cover: 'images/Country Boy.jpg'
+        cover: 'https://opadam.github.io/map/img/Country Boy.jpg'
     },
     {
         name: 'For the Good Times',
         path: 'https://opadam.github.io/map/map/a4',
         artist: 'Albert West',
-        cover: 'images/For the Good Times.jpg'
+        cover: 'https://opadam.github.io/map/img/For the Good Times.jpg'
     },
     {
         name: 'Rain And Tears',
         path: 'https://opadam.github.io/map/map/a5',
         artist: 'Aphrodites Child',
-        cover: 'images/Rain And Tears.jpg'
+        cover: 'https://opadam.github.io/map/img/Rain And Tears.jpg'
     },
 ]
 
