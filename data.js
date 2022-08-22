60a124cbe1db5c8fb8cbe81b16983562ddec5afb
opadam <adam.broeken@gmail.com>
2022-08-22 23:08:36 +0200

    _

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ let songs = [
         name: 'Feel Free',
         path: 'https://opadam.github.io/map/map/a8',
         artist: 'Bellamy Brothers',
-        cover: 'https://opadam.github.io/map/img/Bellamy'
+        cover: 'https://opadam.github.io/map/img/Feel_Free'
     },
     {
         name: 'Candida',
@@ -21,43 +21,43 @@ let songs = [
         name: 'Dance For Ever More',
         path: 'https://opadam.github.io/map/map/a7',
         artist: 'Si Cranstoun',
-        cover: 'https://opadam.github.io/map/img/Si Cranstoun'
+        cover: 'https://opadam.github.io/map/img/Si_Cranstoun'
     },
     {
         name: 'You Drive Me Crazy',
         path: 'https://opadam.github.io/map/map/a6',
         artist: 'Shakin Stevens',
-        cover: 'https://opadam.github.io/map/img/Shakin Stevens'
+        cover: 'https://opadam.github.io/map/img/Shakin_Stevens'
     },
     {
         name: 'Take on Me',
         path: 'https://opadam.github.io/map/map/a1',
         artist: 'A-ha',
-        cover: 'https://opadam.github.io/map/img/Take on Me'
+        cover: 'https://opadam.github.io/map/img/Take_on_Me'
     },
     {
         name: 'You (DU)',
         path: 'https://opadam.github.io/map/map/a2',
         artist: 'Bouke',
-        cover: 'https://opadam.github.io/map/img/You DU'
+        cover: 'https://opadam.github.io/map/img/You_DU'
     },
     {
         name: 'Country Boy',
         path: 'https://opadam.github.io/map/map/a3',
         artist: 'Alan Jackson',
-        cover: 'https://opadam.github.io/map/img/Country Boy'
+        cover: 'https://opadam.github.io/map/img/Country_Boy'
     },
     {
         name: 'For the Good Times',
         path: 'https://opadam.github.io/map/map/a4',
         artist: 'Albert West',
-        cover: 'https://opadam.github.io/map/img/For the Good Times'
+        cover: 'https://opadam.github.io/map/img/For_the_Good_Times'
     },
     {
         name: 'Rain And Tears',
         path: 'https://opadam.github.io/map/map/a5',
         artist: 'Aphrodites Child',
-        cover: 'https://opadam.github.io/map/img/Rain And Tears'
+        cover: 'https://opadam.github.io/map/img/Rain_And_Tears'
     },
 ]
 
